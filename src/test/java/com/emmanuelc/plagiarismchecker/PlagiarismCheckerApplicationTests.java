fd7dda4895ff413d4af2f6e68d56d1d336004cfc
euchigere <ugochigere02gmail.com>
2021-10-21 22:19:38 +0100

    project setup

---
+++ src/test/java/com/emmanuelc/plagiarismchecker/PlagiarismCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emmanuelc.plagiarismchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlagiarismCheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
