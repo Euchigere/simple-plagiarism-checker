f6f7d64f3d0af6e265e9388b101017a966b60446
euchigere <ugochigere02gmail.com>
2021-10-23 13:33:32 +0100

    create base model

---
+++ src/main/java/com/emmanuelc/plagiarismchecker/domain/models/AuditingEntity.java
@@ -0,0 +1,29 @@
+package com.emmanuelc.plagiarismchecker.domain.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class AuditingEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
