e340927b25ec3ef31e1c1b185348246fc07c7bf3
euchigere <ugochigere02gmail.com>
2021-10-23 13:54:08 +0100

    clean up code

--- src/main/java/com/emmanuelc/plagiarismchecker/security/JwtTokenFilter.java
+++ src/main/java/com/emmanuelc/plagiarismchecker/security/JwtTokenFilter.java
@@ -1,6 +1,5 @@
 package com.emmanuelc.plagiarismchecker.security;
 
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
